8971b188d8e8ace6fb19cecff551d24c4dd9b775
Ləman Mustafazadə <126820597+Leman-Mustafazadeh@users.noreply.github.com>
2023-09-09 13:42:33 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,152 @@
+// //a misal
+// var a = 729;
+// var b = -5126;
+// var c = -629;
+// var d = 5126
+ 
+
+// var z = a+b+c+d
+// console.log(z)
+
+// //b misal
+// var k = -638 +(-173)+ 538 + 173
+// console.log(k)
+
+// //c misal
+//  var f = (-372) + 984 + 172 + (-2084)
+//  console.log(f)
+
+//  // ci misal
+//  var g = -78 + 36 + 19 + (-22) + (-25)
+//  console.log(g)
+
+//  // d misal
+//  var r = 125;
+//  var t = -25;
+//  var p = 100;
+//  var j = (-2)
+
+//  var l = r%t+p*j
+//  console.log(l)
+
+//  // e misal
+//  var x = 725;
+//  var y = -5;
+//  var b = 42;
+//  var m = -21;
+//  var v = x/y-b/m
+//  console.log(v)
+
+//  //' misal
+//  var h = 1200 / (-60) - 64 / (-8)
+//  console.log(h);
+
+// // 162ci misal
+
+// //  a  )))
+// var b = 9;
+// var y = 8;
+// var a = 4;
+// var b = 7;
+// var x = 5
+
+// var h = 2+ (b-4)
+// console.log(h)
+
+// // b  ))))
+// var w = - (3 - y) + 7
+// console.log(w)
+
+// // c )))
+// var u = (3 + y) - 42
+// console.log(u)
+
+// // d ))
+// var t = 2 *( x + y ) - 2*x
+// console.log(t);
+
+// // ci ))
+// var z = (x - 5) + (x + 7)
+// console.log(z);
+
+// // e ))
+// var g = 4 * (a - 2*b) + 8 * b 
+// console.log(g);
+
+
+// +    
+
+    // readline.question(, yash => {
+    //     if(yash>18){
+    //         console.log("yasin 18den boyukdur")
+    //     }else if(yash==18){
+    //         console.log("18e beraberdi");
+    //     }
+    //     else{
+    //         console.log("yasin 18den kicikdir")
+    //     }
+    //     readline.close()
+    // });
+    
+
+
+
+
+    // for(i=0;i<100;i++){
+        
+    //     if(i=0 && i<100){
+    //         console.log("sayi 0 ile 100 arasinda");
+    //     }else{
+    //         console.log("sayi 0 ile 100 arasinda degil");
+    //     }
+    // }   
+
+
+
+    // for(i=0;i<)
+
+    // //2ci
+    // if(i%2==0){
+    //     console.log("cift");
+    // }else{
+    //     console.log("tek");
+    // }
+
+
+   
+
+    // //3cu
+    // readline.question('How old are you? What is your name?', yash => {
+    //     if(yash>18){
+    //         console.log("Bu siteye erisim iznini var")
+    //     }else if(yash==18){
+    //         console.log("18e beraberdi");
+    //     }
+    //     else{
+    //         console.log("Bu siteye erisim izniniz yok")
+    //     }
+    //     readline.close()
+    // });
+
+
+
+    // //5ci
+    //  for(i=0;i<100;i++){
+    //     if(90<i<100){
+    //         console.log("AA");
+    //     }else if(80<i<89){
+    //         console.log("BA");
+    //     }else if(70<i<79){
+    //         console.log("BB");
+    //     }else if(60<i<69){
+    //         console.log("CB");
+    //     }else if(50<i<59){
+    //         console.log("CC");
+    //     }else if(40<i<49){
+    //         console.log("DC");
+    //     }else if(30<i<39){
+    //         console.log("DD");
+    //     }else if(0<i<29){
+    //         console.log("FF");
+    //     }
+    //  }
